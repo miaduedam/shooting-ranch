ba6342524b37e136a0889c6e329fbf3c63a4b7a4
Mia Duedam <duedamm@gmail.com>
2026-03-12 15:48:55 +0100

    Added barn scoreboard with UI, script and materials

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Linq;
+using TMPro;
+using System.Collections.Generic;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreBoardText;
+    private List<float> bestTimes = new List<float>();
+    private float latestTime = -1f;
+
+    private void Start()
+    {
+        //UpdateBoard();
+    AddNewTime(14.32f);
+    AddNewTime(12.87f);
+    AddNewTime(15.91f);
+    AddNewTime(11.45f);
+ 
+        
+    }
+
+    public void AddNewTime(float newTime)
+    {
+        latestTime = newTime;
+
+        bestTimes.Add(newTime);
+        bestTimes = bestTimes.OrderBy(time => time).Take(3).ToList();
+        
+        UpdateBoard();
+    }
+
+    private void UpdateBoard()
+    {
+        string text = "BARN RECORDS\n\n";
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(i < bestTimes.Count)
+            {
+                text += $"{i + 1}. {bestTimes[i]:F2}s\n";
+            }
+            else
+            {
+            text += $"{i + 1}. --.--s\n";
+            }
+        }
+        
+        text += "\nLATEST\n";
+
+            if (latestTime >= 0)
+            {
+                text += $"{latestTime:F2}s";
+            }
+            else
+            {
+                text += "--.--s";
+            }
+
+            scoreBoardText.text = text;
+    }
+
+}
